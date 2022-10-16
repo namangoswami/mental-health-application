99db4fc59156d0a822ede926631857f165eaef34
Naman Goswami <namansuman01@gmail.com>
2022-10-16 14:40:13 +0530

    Integrate Mood tracker

--- src/Pages/route-parent/RouteParent.jsx
+++ src/Pages/route-parent/RouteParent.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Wrapper } from "./RouteParent.style";
 import { Outlet } from "react-router-dom";
-import Navbar from '../../Components/Navbar/Navbar.jsx'
-import Sidebar from '../../Components/Sidebar/Sidebar.jsx'
+import Navbar from './Navbar.jsx'
+import Sidebar from './Sidebar.jsx'
 
 function RouteParent() {
 	return (
